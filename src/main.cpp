6d2d7a340a68a3acad1829e4a9a9f21f52b048bf
Mehmet Ulrich <mehmetulrich@gmail.com>
2020-07-31 18:37:02 +0200

    start comment & empth setup

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,14 @@
+/* LED Light with WS2812b
+   It is a LED Matrix with 8x8 single LED's
+   The colors are chosen with 3 Potentiometers in HSV
+*/
 #include <Arduino.h>
 
 void setup(){
-
+	Serial.begin(9600);
+	Serial.write("Start initialization");
+	//TODO initialization
+	Serial.write("initialization finished, starting loop");
 }
 
 void loop(){
