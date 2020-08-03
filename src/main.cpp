fc9224d5b6ed547ac5b37949db7e243646e51ebe
Mehmet Ulrich <mehmetulrich@gmail.com>
2020-08-03 23:17:23 +0200

    nehme einen stm32 statt arduino weil mehr vorhanden

--- src/main.cpp
+++ src/main.cpp
@@ -2,28 +2,29 @@
    It is a LED Matrix with 8x8 single LED's
    The colors are chosen with 3 Potentiometers in HSV
 */
-#include <Arduino.h>
-#include "FastLED.h"
+//#include <Arduino.h>
+//#include "FastLED.h"
 
-#define NUM_LEDS 1
+//#define NUM_LEDS 1
+//#define DATA_PIN 3
 
-#define DATA_PIN 3
-CRGB leds{NUM_LEDS};
+//Array of LED's
+//CRGB leds[ NUM_LEDS ];
 
 void setup(){
-	Serial.begin(9600);
-	Serial.write("Start initialization");
-	FastLED.addLeds<WS2812B, DATA_PIN. GBR>(leds, NUM_LEDS);
+	//Serial.begin(9600);
+	//Serial.write("Start initialization");
+	//FastLED.addLeds<WS2812B, DATA_PIN, GBR>(leds, NUM_LEDS);
 	//TODO initialization
-	Serial.write("initialization finished, starting loop");
+	//Serial.write("initialization finished, starting loop");
 }
 
 void loop(){
-	leds[0] = CRGB::Red;
-	FastLED.show();
-	delay(500);
-	leds[0] = CRGB::Black;
-	FastLED.show();
-	delay(500);
+	//leds[0] = CRGB::Red;
+	//FastLED.show();
+	//delay(500);
+	//leds[0] = CRGB::Black;
+	//FastLED.show();
+	//delay(500);
 
 }
