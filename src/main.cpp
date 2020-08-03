8db99cac5468b0004a02c120302e14a25c490a2a
Mehmet Ulrich <mehmetulrich@gmail.com>
2020-08-03 15:27:29 +0200

    mehh

--- src/main.cpp
+++ src/main.cpp
@@ -3,14 +3,27 @@
    The colors are chosen with 3 Potentiometers in HSV
 */
 #include <Arduino.h>
+#include "FastLED.h"
+
+#define NUM_LEDS 1
+
+#define DATA_PIN 3
+CRGB leds{NUM_LEDS};
 
 void setup(){
 	Serial.begin(9600);
 	Serial.write("Start initialization");
+	FastLED.addLeds<WS2812B, DATA_PIN. GBR>(leds, NUM_LEDS);
 	//TODO initialization
 	Serial.write("initialization finished, starting loop");
 }
 
 void loop(){
+	leds[0] = CRGB::Red;
+	FastLED.show();
+	delay(500);
+	leds[0] = CRGB::Black;
+	FastLED.show();
+	delay(500);
 
 }
