71f017f68fc363943f8fc86c11ecbff1d7b10051
Mehmet Ulrich <mehmetulrich@gmail.com>
2020-08-11 16:02:52 +0200

    formattet serial value print

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,7 @@
 #include "FastLED.h"
 
 #define NUM_LEDS 1
-#define DATA_PIN 13
+#define DATA_PIN 2
 #define BRIGHTNESS 90
 #define COLOR_ORDER GBR
 #define LED_TYPE WS2812B
@@ -25,6 +25,17 @@ int green_value;
 int blue_poti = A2;
 int blue_value;
 
+void set_Color_RGB(int r, int g, int b) {
+	CRGB color = CRGB(r, g, b);
+	fill_solid(leds, NUM_LEDS, color);
+}
+
+void print_values(int r, int g, int b) {
+	char buf[100];
+	sprintf(buf, "Red: %d, Green: %d, Blue: %d", r, g, b);
+	Serial.println(buf);
+}
+
 void setup(){
 	// initialize LED digital pin as an output
 	//pinMode(LED_BUILTIN, OUTPUT);
@@ -34,6 +45,11 @@ void setup(){
 
 void loop(){
 	red_value = analogRead(red_poti);
-	//Serial.println(red_value);
-
+	red_value = map(red_value, 0, 1023, 0, 255);
+	green_value = analogRead(green_poti);
+	green_value = map(green_value, 0, 1023, 0, 255);
+	blue_value = analogRead(blue_poti);
+	blue_value = map(blue_value, 0, 1023, 0, 255);
+	set_Color_RGB(red_value, green_value, blue_value);
+	print_values(red_value, green_value, blue_value);
 }
